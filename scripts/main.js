582702c0d28b2bd3b6a6b2c771d42fd691af0c92
Keziah T. <keziahnibu@gmail.com>
2026-06-05 14:18:51 -0500

    Light cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,6 @@ const ctx = canvas.getContext("2d");
 
 const environment = new Environment(ctx, window.innerWidth, window.innerHeight);
 
-const objects = [];
-
 function resize() {
   canvas.height = environment.height = window.innerHeight;
   canvas.width = environment.width = window.innerWidth;
@@ -16,15 +14,12 @@ function drawScene() {
   environment.draw(ctx, canvas.width, canvas.height);
 }
 
-function calculateActions() {}
-
 function init() {
   resize();
 }
 
 function gameLoop() {
   resize();
-  calculateActions();
   drawScene();
   requestAnimationFrame(gameLoop);
 }
